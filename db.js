60f156c2bfce9aed7a5638e9ffa6eb29d6c2ae51
Jung Hong <junghong0512@gmail.com>
2019-12-18 19:01:01 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,54 +1,14 @@
-export const videos = [
-  {
-    id: 40124,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 9956,
-      name: "Hong",
-      email: "jung@hong.com"
-    }
-  },
-  {
-    id: 40241,
-    title: "Video super",
-    description: "This is something I love",
-    views: 40,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 9956,
-      name: "Hong",
-      email: "jung@hong.com"
-    }
-  },
-  {
-    id: 44320,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 13,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 7729,
-      name: "Jung",
-      email: "hong@jung.com"
-    }
-  },
-  {
-    id: 42034,
-    title: "Video perfet",
-    description: "This is something I love",
-    views: 64,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 3276,
-      name: "Yeun",
-      email: "yeun@hong.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connented to DB");
+const handleError = error => console.log(`Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
