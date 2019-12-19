240775b0af8dd899123b156017f38fc9ee32986e
Jung Hong <junghong0512@gmail.com>
2019-12-19 13:48:42 +0900

    Uploading and Creating a Video

--- middleware.js
+++ middleware.js
@@ -1,4 +1,8 @@
+import multer from "multer";
 import routes from "./routes";
+
+// middleware to get file URL(경로)
+const multerVideo = multer({ dest: "uploads/videos/" }); // server에 upload/videos 라는 folder에 저장할것
 
 export const localMiddleware = (req, res, next) => {
   // locals에 로컬 변수를 저장하면, 이 변수를 template에서 사용할 수 있다.
@@ -10,3 +14,5 @@ export const localMiddleware = (req, res, next) => {
   };
   next(); // middleware가 connection 과 route들 사이에 있기 때문에
 };
+
+export const uploadVideo = multerVideo.single("videoFile"); // single: upload only 1 file, "name" of the file to bring
