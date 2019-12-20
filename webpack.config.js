4b846d49f9c6766736eafb34ebf5f06afe2893d1
Jung Hong <junghong0512@gmail.com>
2019-12-20 16:26:38 +0900

    installing Webpack and Styles with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path"); // file and directory 를 absolute 하게 만들어줌(서버에서 전체 경로를 갖는다)
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ENTRY_FILE,
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
+          {
+            loader: "css-loader" // how to webpack understand CSS
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              }
+            }
+          },
+          {
+            loader: "sass-loader"
+          }
+        ])
+      }
+    ]
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js"
+  },
+  plugins: [new ExtractCSS("styles.css")]
+};
+
+module.exports = config;
