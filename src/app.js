1da544c2ca13d96ceb7473c2dd6540bab54e3f34
Jung Hong <junghong0512@gmail.com>
2020-01-03 18:42:59 +0900

    building for Production

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import mongoose from "mongoose";
 import session from "express-session";
+import path from "path";
 import MongoStore from "connect-mongo";
 import { localMiddleware } from "./middleware";
 import routes from "./routes";
@@ -23,8 +24,10 @@ const CokieStore = MongoStore(session);
 
 app.use(helmet()); // 앱보안을 위해서 추가됨(특별한 기능은 없다)
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads")); // directory 에서 file 을 내보내주는 middleware(expose a foler to the internet)
-app.use("/static", express.static("static")); // static route 를 추가한다.(/static 이라는 라우터가 존재하지 않기 때문에), static router에 접근하면 static folder로 유도한다
+app.set("views", path.join(__dirname, "views"));
+// 제거 app.use("/uploads", express.static("uploads")); // directory 에서 file 을 내보내주는 middleware(expose a foler to the internet)
+// 변경 app.use("/static", express.static("static")); // static route 를 추가한다.(/static 이라는 라우터가 존재하지 않기 때문에), static router에 접근하면 static folder로 유도한다
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser()); // cookie를 전달 받아서 사용할 수 있도록 만들어주는 middleware
 app.use(bodyParser.json()); // body parser는 form 데이터 가진 request object에 접근 (req.body 로 전송된 데이터에 접근할 수 있다)
 app.use(bodyParser.urlencoded({ extended: true })); // server가 user로부터 받은 data 를 다루는 방법
