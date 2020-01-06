3fc37146ef91d90d3f14058fd0c9afc6f5708b7c
Jung Hong <junghong0512@gmail.com>
2020-01-06 22:40:28 +0900

    babel/cli installed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,7 +9,7 @@ export const postJoin = async (req, res, next) => {
     body: { name, email, password, password2 }
   } = req;
   if (password !== password2) {
-    req.flash("error", "Passwords don't match!");
+    req.flash("error", "Passwords don't match!"); // flash 사용
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
